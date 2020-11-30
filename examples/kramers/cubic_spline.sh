ccf4992fdbbe06971f8bd2cd78361b9c135838d0
marc <marc@kubo.ftmc.uam.es>
2020-11-30 23:10:21 +0100

    Deleting extra files

--- examples/kramers/cubic_spline.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for D in `seq 0.2 0.2 2`; do ./cubic_spline 1 1 $D 1; done | grep -v "#" > mu_vs_D.dat
-for R in `seq 1 0.2 3`; do ./cubic_spline 1 $R 1 1; done | grep -v "#" > mu_vs_R.dat
-for E0 in `seq 0.25 0.25 7.5`; do ./cubic_spline $E0 1 1 1; done | grep -v "#" > mu_vs_E0.dat
